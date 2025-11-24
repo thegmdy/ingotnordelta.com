8c7c6b76502a1524e8e6ab276445a420ef64a7fa
thegmdy <iamgmdy@gmail.com>
2025-11-24 14:18:08 -0600

    Change import paths for Header and Footer components

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const queryClient = new QueryClient({
   defaultOptions: {
