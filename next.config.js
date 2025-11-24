a6331c706afdb83e977b26ff09c193551bb4d019
thegmdy <iamgmdy@gmail.com>
2025-11-24 12:49:33 -0600

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
+      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+};
+
+module.exports = nextConfig;
